e216232269fc7c2ee24124a9f695fd9c774b44be
Zheng Wei <zw.fengren@gmail.com>
2026-08-31 17:54:08 +0800

    fix: merge quotation migration branches

---
+++ backend/quotation/migrations/0029_merge_quotation_heads.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("quotation", "0027_defer_draft_quote_numbering"),
+        ("quotation", "0028_export_attachment_selection"),
+    ]
+
+    operations = []
